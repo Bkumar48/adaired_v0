e916ac48b5ae58b2cf5ae16aa17b30d02bdd7f1e
Kumar <bittu.kumar@adaired61.com>
2023-11-01 20:05:12 +0530

    admin panel started (01-11)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./views/frontend/pages/HomePage";
 import Header from "./views/frontend/global/header/Header";
 import Footer from "./views/frontend/global/footer/Footer";
-import "./App.css";
-const App = () => {
+
+// Admin Panel Imports
+import SideBar from "./views/adminpanel/components/sidebar/SideBar";
+import DashBoard from "./views/adminpanel/pages/DashBoard";
+import "./views/adminpanel/assets/css/index.css";
+import "./views/adminpanel/assets/css/index.css";
+import TopNav from "./views/adminpanel/components/topnav/TopNav";
+
+const App = (props) => {
+  useEffect(() => {
+    document.title =
+      window.location.pathname === "/admin"
+        ? "Dashboard | Adaired"
+        : "Home | Adaired";
+  }, []);
+
   return (
     <>
       <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-        <Footer />
+        <div className="layout">
+          {!window.location.pathname.startsWith("/admin") && <Header />}
+          {window.location.pathname.startsWith("/admin") && (
+            <SideBar {...props} />
+          )}
+          {window.location.pathname.startsWith("/admin") && <TopNav />}
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route exact path="/admin" element={<DashBoard />}>
+              <Route index element={<DashBoard />} />
+              <Route path="dashboard" element={<DashBoard />} />
+            </Route>
+          </Routes>
+          {!window.location.pathname.startsWith("/admin") && <Footer />}
+        </div>
       </Router>
     </>
   );
